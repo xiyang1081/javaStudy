2867af9f18c8f780464e2e01ef8f4aa8791ec0a7
夕阳 <linandli@yeah.net>
2016-01-21 23:28:25 +0800

    添加20160121学习数据

---
+++ 20160121/demo.java
@@ -0,0 +1,15 @@
+/*
+@author 夕阳
+各个进制展示以及转换
+*/
+
+class demo {
+	public static void main(String[] args) {
+		System.out.println(0x10);  //16进制
+		System.out.println(010);  //8进制
+
+		System.out.println("十进制转二进制:" 
+			+ Integer.toBinaryString(10)); //十进制转二进制
+
+	}
+}
